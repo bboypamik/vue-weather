d1d7b296b5f0761bb597526d52dee0be27eb3837
Miroslav Pantos <bboypamik@gmail.com>
2021-01-18 20:16:43 +0100

    change picture based on temperature. Finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input type="text"
@@ -82,6 +82,10 @@ export default {
     background-size: cover;
     background-position: bottom;
     transition: 0.4s;
+  }
+
+  #app.warm {
+    background-image: url('./assets/warm-bg.jpg')
   }
 
   main {
