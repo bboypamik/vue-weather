65f1dc130262bb480bd7f5dbf6f0de0a84ca9606
Miroslav Pantos <bboypamik@gmail.com>
2021-01-18 20:07:27 +0100

    time and date done....

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}} </div>
-          <div class="date">Monday 18 January 2021</div>
+          <div class="date">{{dateBuilder()}}</div>
         </div> 
         <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
         <div class="weather">{{weather.weather[0].main}}</div>
@@ -50,6 +50,17 @@ export default {
 
     setResults(results){
       this.weather = results;
+    },
+
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
